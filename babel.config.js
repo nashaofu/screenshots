4578689f5758d8fc401016459fe73ce156b20def
nashaofu <diaocheng@outlook.com>
2019-02-24 16:40:55 +0800

    modify 修复渲染进程打包错误问题

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          chrome: '58',
+          node: 8,
+          electron: 2
+        }
+      }
+    ]
+  ],
   plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: false,
+        helpers: true,
+        regenerator: true,
+        useESModules: false
+      }
+    ],
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-default-from'
   ]
