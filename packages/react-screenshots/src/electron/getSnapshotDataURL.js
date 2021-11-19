48595becd9eab48d3fdc1d46e5e187730c4f460c
nashaofu <diaocheng@outlook.com>
2021-11-19 21:37:02 +0800

    fix: 修复linux上截图获取source错误问题

--- packages/react-screenshots/src/electron/getSnapshotDataURL.js
+++ packages/react-screenshots/src/electron/getSnapshotDataURL.js
@@ -9,12 +9,12 @@ export default async display => {
     }
   })
 
-  let source;
+  let source
   // Linux系统上，screen.getDisplayNearestPoint 返回的 Display 对象的 id 和 这儿 source 对象上的 display_id(Linux上，这个值是空字符串) 或 id 的中间部分，都不一致
   // 但是，如果只有一个显示器的话，其实不用判断，直接返回就行
   if (sources.length === 1) {
-    source = sources[0];
-  } else{
+    source = sources[0]
+  } else {
     source = sources.find(source => {
       return source.display_id === display.id.toString() || source.id.startsWith(`screen:${display.id}:`)
     })
