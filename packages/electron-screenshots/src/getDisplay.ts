c11eec77c31cc83f126518cb593f61036e114bd3
nashaofu <diaocheng@outlook.com>
2022-05-26 09:43:44 +0800

    feat: linux windows 支持 kiosk 模式

---
+++ packages/electron-screenshots/src/getDisplay.ts
@@ -0,0 +1,19 @@
+import { Rectangle, screen } from 'electron'
+
+export interface Display extends Rectangle {
+  id: number
+}
+
+export default (): Display => {
+  const point = screen.getCursorScreenPoint()
+  const { id, bounds } = screen.getDisplayNearestPoint(point)
+
+  // https://github.com/nashaofu/screenshots/issues/98
+  return {
+    id,
+    x: Math.floor(bounds.x),
+    y: Math.floor(bounds.y),
+    width: Math.floor(bounds.width),
+    height: Math.floor(bounds.height)
+  }
+}
